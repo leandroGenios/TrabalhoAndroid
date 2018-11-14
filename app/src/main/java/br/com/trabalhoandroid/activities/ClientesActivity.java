d93dabaf873c57f14132de4fdf8c6c107cdf847a
leandroGenios <leandro.genios@gmail.com>
2018-11-14 12:49:08 -0200

    .

--- app/src/main/java/br/com/trabalhoandroid/activities/ClientesActivity.java
+++ app/src/main/java/br/com/trabalhoandroid/activities/ClientesActivity.java
@@ -1,6 +1,7 @@
 package br.com.trabalhoandroid.activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,8 +13,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import br.com.trabalhoandroid.adapter.ClienteListAdapter;
 import br.com.trabalhoandroid.trabalhoandroid.R;
+import br.com.trabalhoandroid.utils.Network;
 
 public class ClientesActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
@@ -30,6 +37,24 @@ public class ClientesActivity extends AppCompatActivity implements AdapterView.O
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(onClickClientes());
+
+        //Network.getJson("http://192.168.1.5:8080/ServidorRest/rest/clientes");
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                // Create URL
+                URL githubEndpoint = null;
+                try {
+                    githubEndpoint = new URL("https://api.github.com/");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                // Create connection
+                HttpsURLConnection myConnection = (HttpsURLConnection) githubEndpoint.openConnection();
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/br/com/trabalhoandroid/models/Cliente.java
@@ -0,0 +1,4 @@
+package br.com.trabalhoandroid.models;
+
+public class Cliente {
+}
